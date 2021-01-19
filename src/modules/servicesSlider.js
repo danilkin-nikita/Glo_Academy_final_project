6765e4db996e08a25c5574b929bdc86574dc85bd
Nikita Danilkin <ni.danilkin@gmail.com>
2021-01-19 14:28:58 +0300

    доработал слайдер карусель

--- src/modules/servicesSlider.js
+++ src/modules/servicesSlider.js
@@ -3,21 +3,31 @@
 // слайдер карусель услуг
 const servicesSlider = () => {
   const slider = document.querySelector('.services-slider'),
-        slide = slider.querySelectorAll('.slide');
-        
+        slide = slider.querySelectorAll('.slide'),
+        sliderBtn = slider.querySelectorAll('.slider-arrow');
 
   let position = 0,
       slidesToShow = 5;
 
   const checkWindow = () => {
-    if (window.innerWidth >= 1141) {
+    position = 0;
+
+    if (window.innerWidth >= 1200) {
       slidesToShow = 5;
-    } else if (window.innerWidth < 1141 && window.innerWidth >= 767) {
-      slidesToShow = 3;
-    } else if (window.innerWidth < 767 && window.innerWidth >= 500) {
-      slidesToShow = 2;
-    } else if (window.innerWidth < 479) {
-      slidesToShow = 1;
+      sliderBtn.forEach(item => {
+        item.style.display = 'none';
+      });
+    } else {
+      sliderBtn.forEach(item => {
+        item.style.display = 'block';
+      })
+      if (window.innerWidth < 1200 && window.innerWidth >= 767) {
+          slidesToShow = 4;
+        } else if (window.innerWidth < 767 && window.innerWidth >= 479) {
+          slidesToShow = 2;
+        } else if (window.innerWidth < 479) {
+          slidesToShow = 1;
+        }
     }
     
     for (let i = 0; i < slide.length; i++) {
@@ -26,14 +36,14 @@ const servicesSlider = () => {
     for (let i = 0; i < slidesToShow; i++) {
       slide[i].classList.add('active-services-slide');
     }
-  }
+    checkBtn();
+  };
 
   const addActiveSlide = () => {
     
     for (let i = 0; i < slidesToShow; i++) {
       slide[i].classList.add('active-services-slide');
     }
-
     checkWindow();
   };
 
@@ -47,25 +57,39 @@ const servicesSlider = () => {
     elem[index].classList.remove(strClass);
   };
 
+  const checkBtn = () => {
+    sliderBtn.forEach(item => {
+      item.style.display = 'block';
+    });
+    if (position === (slide.length - slidesToShow)) {
+        sliderBtn[1].style.display = 'none';
+    }
+    if (position === 0) {
+      sliderBtn[0].style.display = 'none';
+    }
+  };
+
   document.addEventListener('click', event => {
     let target = event.target;
 
     if (target.closest('.next')) { 
       if (position < (slide.length - slidesToShow)) {
         nextSlide(slide, position, 'active-services-slide');
-        position++; 
+        position++;
       }
+      checkBtn();
     }
     if (target.matches('.prev')) {
       if (position > 0) {
         position--;
       }
       prevSlide(slide, position, 'active-services-slide');
+      checkBtn();
     }
   });
 
   window.addEventListener('resize', () => {
-    checkWindow()
+    checkWindow();
   });
 
   addActiveSlide();
