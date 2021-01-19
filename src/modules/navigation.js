65c40d4ce55e056b8d8159bb23a225112be09925
Nikita Danilkin <ni.danilkin@gmail.com>
2021-01-19 15:14:56 +0300

    добавил плавный скролл

--- src/modules/navigation.js
+++ src/modules/navigation.js
@@ -6,11 +6,29 @@ const arrowAppearance = () => {
 
   toTop.style.display = 'none';
 
+  const moveToAnchor = item => {
+    const blockID = item.getAttribute('href').substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  };
+
   document.addEventListener('scroll', () => {
     if (pageYOffset >= 620) {
       toTop.style.display = 'block';
     } else {
       toTop.style.display = 'none';
+    }
+  });
+
+  document.addEventListener('click', event => {
+    let target = event.target;
+
+    if (target.closest('nav') && target.closest('a') || target.closest('#totop') || target.closest('.about') || target.closest('.for-clients')) {
+      event.preventDefault();
+      moveToAnchor(target.closest('a'));
     }
   });
 };
